bdaff0c83105cc0acf40efccc7846040f47d96cc
ACHIYANT <achiyant@gmail.com>
2023-09-07 20:54:40 +0530

    Added config

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+  // api_key:"eLfFKSVY10rm4ULwdlYBsevjRztLQzoO",
+  // api_key: "Tl5Y8YpbiNRrxpd9JGUZrbwj5I3C3RGj",
+  // api_key: "CHBERp7YXAW5TlTPWGva5rTEV4JvjOEm",
+  api_key: "cgjHFl6uHLAYyN5Tzhpx9SGMUl84Gya8",
+  // api_key: "cZjSnawVVuwWKrSvKXmYOeOu4gs1F5b9",
+  // api_key: "lhMuhYPPTL39rkLC901fPVEaxK8zpwhH",
+  // api_key: "3Q5lVF0AABfZ5uaHHOT7EVCIlvqBTLVU",
+  // api_key: "GL1sSzAETgCPAdXrL2AyLsgVYu19s5PR",
+  // api_key: "hW60sG0AZCG1V63AsBfqyLANB2c5Pv97",
+};
